6a47bba2c8728d12def7edca5eea6214df96954e
Wassim-Rached <wassimrached404@gmail.com>
2023-11-15 11:29:50 +0100

    tp8

--- tp8/src/main/java/produit/Article.java
+++ tp8/src/main/java/produit/Article.java
@@ -1,5 +1,41 @@
 package produit;
 
 public class Article {
+	protected String Code;
+	protected String Designation;
+	protected Double Prix;
+	protected Double TVA;
+	protected String Famille;
+
+	public Article(String Code, String Designation, Double Prix, Double TVA, String Famille) {
+		this.Code = Code;
+		this.Designation = Designation;
+		this.Prix = Prix;
+		this.TVA = TVA;
+		this.Famille = Famille;
+	}
+
+	@Override
+	public String toString() {
+		return "Article [Code=" + Code + ", Designation=" + Designation + ", Prix=" + Prix + ", TVA=" + TVA
+				+ ", Famille=" + Famille + "]";
+	}
+
+	public boolean memefamille (Article article){
+		return this.Famille.equals(article.Famille);
+	}
+
+	public double PrixTTC(){
+		return this.Prix * (1 + this.TVA);
+	}
+
 	
+	public boolean equals(Article article){
+		return this.Code.equals(article.Code);
+	}
+
+	public String getCode(){
+		return this.Code;
+	}
+
 }
--- tp8/src/main/java/produit/ArticleLot.java
+++ tp8/src/main/java/produit/ArticleLot.java
@@ -1,5 +1,25 @@
 package produit;
 
-public class ArticleLot {
-	
+public class ArticleLot extends Article{
+	private int nombreDePieces;
+	private double remise;
+
+	public ArticleLot(String Code, String Designation, Double Prix, Double TVA, String Famille, int nombreDePieces, double remise) {
+		super(Code, Designation, Prix, TVA, Famille);
+		this.nombreDePieces = nombreDePieces;
+		this.remise = remise;
+	}
+
+	@Override()
+	public String toString() {
+		return super.toString() + " ArticleLot [nombreDePieces=" + nombreDePieces + ", remise=" + remise + "]";
+	}
+
+	public double PrixTTC() {
+		return this.Prix * (1 + this.TVA) * (1 - this.remise );
+	}
+
+	public int getNombreDePieces() {
+		return this.nombreDePieces;
+	}
 }
